293fd5d3fa5a4ff64f4084b6b09cd27e839e5dae
Bruce Becker <brucellino@protonmail.ch>
2025-03-16 07:54:36 +0100

    feat: add a panel
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ resource "grafana_dashboard" "nodes" {
   config_json = file("${path.module}/dashboards/nodes.json")
   folder      = grafana_folder.hah.id
 }
+
+resource "grafana_library_panel" "disk_free" {
+  name       = "Node Disk Free"
+  folder_uid = grafana_folder.hah.uid
+  uid        = "hah-disk-node-free"
+  model_json = file("${path.module}/library_panels/node-disk-free.json")
+}
